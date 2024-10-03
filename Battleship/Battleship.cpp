b53dda4483bbf533349c5fc6efe55d8ccd7d25ee
lvallone3315 <55007696+lvallone3315@users.noreply.github.com>
2024-10-03 11:46:14 -0400

    Added strings in main for intro message & sprint one addendum. implemented UI class displayMessage method Added stubs for the remaining UI class methods Updated main() to display the intro & addendum
    Issues - single quotes not displaying properly, may need to change encoding to unicode?
    
    ToDo - tech debt - refactor string constants into own header file

--- Battleship/Battleship.cpp
+++ Battleship/Battleship.cpp
@@ -6,17 +6,52 @@
 
 #include <iostream>
 #include <stdlib.h>
-
+#include <string>
 
 #include "BattleshipBoard.h"
 #include "BattleshipPlayer.h"
+#include "BattleshipUI.h"
+
+using namespace std;
+
+
 
 int main()
 {
+    // ToDo - move this into a separate class or header file
+const string introMessage = R"(Battleship (also known as Battleships) is a strategy type guessing game for two players.
+The game is played on ruled grids. Each player has a grid on which the player’s fleet of warships is marked. 
+The locations of the fleets are concealed from the other player. 
+Players alternate turns calling "shots" at the other player's ships.
+The objective of the game is to destroy the opposing player's fleet.
+
+Each player has a second grid mimicking their opponent’s grid to keep track of shots fired
+ at the other player’s warships and whether those shots hit a ship or missed(landed harmlessly in the water).
+
+The game is played on an 10x10 grid with battleship sizes ranging from two grid cells to six grid cells.
+
+The cells are referenced by column letter(A - J) and row number(1 - 10).
+Opponents take turns attacking by calling out a cell (e.g.B4 = 2nd column, 4th row)
+
+The defender responds with “hit”(a warship occupies the cell) or “miss”.
+If the attacker shoots all of the cells a warship occupies, the defender responds with "you sunk my battleship".
+
+The defender’s primary grid will show their battleship as well as hits.
+he attacker’s secondary grid will also keep track of the cells mentioned and the hits / misses 
+  (for strategic purposes, as well as to avoid calling cells already mentioned).
+
+The game is over when all of the warships belonging to either player are "sunk")";
+
+const string sprintOneMessage = R"(-------------
+For this first version of FDU Battleships, 
+  there will be a 5x5 grid (A1 to E5) and the computer will place a 3 cell battleship on its grid.
+
+To win, the human player must sink the battleship in 10 turns or less.
+
+Good hunting!)";
+
     std::cout << "Commence Firing!\n";
 
-    // BattleshipPlayer humanPlayer;
-    // 
     // create two grids (e.g. boards) each for computer player & human player
     //   For each player - one grid will represent the player's warships & shots by opponent
     //       the second grid will represent the shots by the player (to hit the opponents warships)
@@ -24,6 +59,10 @@ int main()
     //      the computer will have a ship, but will not be firing shots
     BattleshipBoard computerBoard;
     BattleshipBoard playerBoard;
+    BattleshipUI console;
+
+    console.displayMessage(introMessage);
+    console.displayMessage(sprintOneMessage);
 
     std::cout << "cell 0 0 = " << playerBoard.getMyShotsGridValue(0, 0) << std::endl;  // replace this with call to UI to display full grid
 
