9088b51c4ce59ade4d6a942e0c14119348726132
lvallone3315 <55007696+lvallone3315@users.noreply.github.com>
2024-10-03 11:46:14 -0400

    Test-160 Added strings in main for intro message & sprint one addendum. implemented UI class displayMessage method Added stubs for the remaining UI class methods Updated main() to display the intro & addendum
    Issues - single quotes not displaying properly, may need to change encoding to unicode?
    
    ToDo - tech debt - refactor string constants into own header file

--- Battleship/BattleshipUI.cpp
+++ Battleship/BattleshipUI.cpp
@@ -1,1 +1,26 @@
+#include <stdlib.h>
+#include <iostream>
 #include "BattleshipUI.h"
+
+BattleshipUI::BattleshipUI() {
+	// empty constructor - nothing to do for now
+}
+
+// display methods will use the passed in player object to print out the appropriate board
+void BattleshipUI::displayMyShots(BattleshipBoard playerGrid) {
+	// To Do - to be implemented
+}
+void BattleshipUI::displayOppsShots(BattleshipBoard playerGrid) {
+	// To Do - to be implemented
+}
+
+// displaying a message on the console currently assumes all relevant info is passed in
+void BattleshipUI::displayMessage(string messageToBeDisplayed) {
+	std::cout << messageToBeDisplayed << std::endl;
+}
+
+// return the players input, will be validated and processed in another class
+string BattleshipUI::getPlayerInput() {
+	// To Do - to be implemented
+	return "";
+}
